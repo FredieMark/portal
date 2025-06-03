574e5ce536612623a3dc0a9cb1ac073a70b3c7f7
fredie <ojibona1@gmail.com>
2025-06-03 19:47:48 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ import Fast from './assets/sprint.svg'
 import Scale from './assets/open_with.svg'
 import Mind from './assets/mindfulness.svg'
 import Paid from './assets/paid.svg'
+import MetaMaskIcon from "./assets/MetaMaskIcon";
+import WalletConnectIcon from "./assets/WalletConnectIcon";
+import CameraLensIcon from "./assets/CameraLensIcon";
+import FrameIcon from "./assets/Frame";
 
 const Header = () => (
   <header className="large">
@@ -189,6 +193,57 @@ const Main = () => (
           </div>
         ))}
       </div>
+    </section>
+
+    <section className="bScreen">
+      <div className="inScreen">
+        <div className="hScreen">
+          <p>Connect Wallet</p>
+          <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" style={{
+            width: '1rem',
+            height: '1rem'
+          }} >
+          <path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+          </svg>
+        </div>
+        <div className="bdScreen">
+          <button>
+            <p>
+            <span>MetaMask</span>
+            <span>Connect</span>
+            </p>
+            <MetaMaskIcon/>
+          </button>
+          <button>
+          <p>
+            <span>Wallet Connect</span>
+            <span>Connect</span>
+            </p>
+            <WalletConnectIcon/>
+          </button>
+          <button>
+          <p>
+            <span>Coinbase Connect</span>
+            <span>Connect</span>
+            </p>
+            <CameraLensIcon/>
+          </button>
+          <button>
+          <p>
+            <span>Ledger</span>
+            <span>Connect</span>
+            </p>
+            <FrameIcon/>
+          </button>
+          <p style={{
+            fontSize: 15,
+          }}>By connecting a wallet, you agree to the  
+          <span style={{
+            color: 'rgb(230, 32, 88)'
+          }}> Terms of Service</span></p>
+        </div>
+      </div>
+
     </section>
   </main>
 );
