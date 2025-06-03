f44775638aa5c0bbc03684553e4e1c41d5242382
fredie <113792903+FredieMark@users.noreply.github.com>
2025-06-03 19:59:51 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const Header = () => (
       <li>Management</li>
     </nav>
     <div>
-      <button>Connect Wallet</button>
+      <button onClick={OpenModal}>Connect Wallet</button>
     </div>
   </header>
 );
@@ -195,16 +195,18 @@ const Main = () => (
       </div>
     </section>
 
-    <section className="bScreen">
+    <section id="bScreen" className="bScreen">
       <div className="inScreen">
         <div className="hScreen">
           <p>Connect Wallet</p>
+          <p onClick={CloseModal}>
           <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" style={{
             width: '1rem',
             height: '1rem'
           }} >
           <path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
           </svg>
+          </p>
         </div>
         <div className="bdScreen">
           <button>
@@ -321,6 +323,21 @@ const Footer = () => (
     </div>
   </footer>
 );
+
+function OpenModal() {
+  const bScreen = document.getElementById('bScreen');
+  if (bScreen) {
+    bScreen.style.display = 'flex';
+  }
+}
+
+function CloseModal() {
+  const bScreen = document.getElementById('bScreen');
+  if (bScreen) {
+    bScreen.style.display = 'none';
+  }
+}
+
 
 const App = () => (
   <div>
